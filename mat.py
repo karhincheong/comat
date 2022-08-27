25b93c52984e43ef239d7ff6c687abfa23072a5c
Kar Hin <kh030410@gmail.com>
2022-08-27 20:04:17 +0800

    Exception for negative numbers

--- mat.py
+++ mat.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python
 import argparse
 from utilities.formatter.number import *
+from rich import print
 
 parser = argparse.ArgumentParser()
 
 # Number argument
-parser.add_argument("-n", "--number", type=int, help="Enter a number and it will tell you its properties", required=True)
+parser.add_argument("-n", "--number", type=int, help="Enter a non-negative integer and it will tell you its properties", required=True)
 
 args = parser.parse_args()
-print_number(args.number)
+#print_number(args.number)
+if args.number < 0:
+    print("[bold magenta]Mat doesn't support negative integers[/bold magenta]")
+else:
+    print_number(args.number)
